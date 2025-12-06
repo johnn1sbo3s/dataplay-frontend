35a992e86cab232543617e1de72ee9821f0714e1
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-12-06 17:50:03 -0300

    Organiza sidebar, removendo opções não utilizadas do menu

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,92 +1,51 @@
+<!-- eslint-disable @stylistic/indent -->
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui'
 
-const route = useRoute()
 const toast = useToast()
 
 const open = ref(false)
 
-const links = [[{
-  label: 'Home',
-  icon: 'i-lucide-house',
-  to: '/',
-  onSelect: () => {
-    open.value = false
-  }
-}, {
-  label: 'Inbox',
-  icon: 'i-lucide-inbox',
-  to: '/inbox',
-  badge: '4',
-  onSelect: () => {
-    open.value = false
-  }
-}, {
-  label: 'Customers',
-  icon: 'i-lucide-users',
-  to: '/customers',
-  onSelect: () => {
-    open.value = false
-  }
-}, {
-  label: 'Settings',
-  to: '/settings',
-  icon: 'i-lucide-settings',
-  defaultOpen: true,
-  type: 'trigger',
-  children: [{
-    label: 'General',
-    to: '/settings',
-    exact: true,
-    onSelect: () => {
-      open.value = false
+const links = [
+  [
+    {
+      label: 'Início',
+      icon: 'i-lucide-house',
+      to: '/',
+      onSelect: () => {
+        open.value = false
+      }
+    },
+    {
+      label: 'Jogos do dia',
+      icon: 'i-lucide-inbox',
+      to: '/inbox',
+      badge: '435',
+      onSelect: () => {
+        open.value = false
+      }
     }
-  }, {
-    label: 'Members',
-    to: '/settings/members',
-    onSelect: () => {
-      open.value = false
+  ],
+  [
+    {
+      label: 'GitHub',
+      icon: 'i-lucide-github',
+      to: 'https://github.com/johnn1sbo3s',
+      target: '_blank'
+    }, {
+      label: 'LinkedIn',
+      icon: 'i-lucide-linkedin',
+      to: 'https://www.linkedin.com/in/joaopaulo-castro',
+      target: '_blank'
+    },
+    {
+      label: 'Entre em contato',
+      icon: 'i-lucide-mail',
+      to: 'mailto:joaopaulo.castro@gmail.com',
+      target: '_blank'
     }
-  }, {
-    label: 'Notifications',
-    to: '/settings/notifications',
-    onSelect: () => {
-      open.value = false
-    }
-  }, {
-    label: 'Security',
-    to: '/settings/security',
-    onSelect: () => {
-      open.value = false
-    }
-  }]
-}], [{
-  label: 'Feedback',
-  icon: 'i-lucide-message-circle',
-  to: 'https://github.com/nuxt-ui-templates/dashboard',
-  target: '_blank'
-}, {
-  label: 'Help & Support',
-  icon: 'i-lucide-info',
-  to: 'https://github.com/nuxt-ui-templates/dashboard',
-  target: '_blank'
-}]] satisfies NavigationMenuItem[][]
-
-const groups = computed(() => [{
-  id: 'links',
-  label: 'Go to',
-  items: links.flat()
-}, {
-  id: 'code',
-  label: 'Code',
-  items: [{
-    id: 'source',
-    label: 'View page source',
-    icon: 'i-simple-icons-github',
-    to: `https://github.com/nuxt-ui-templates/dashboard/blob/main/app/pages${route.path === '/' ? '/index' : route.path}.vue`,
-    target: '_blank'
-  }]
-}])
+  ]
+] satisfies NavigationMenuItem[][]
 
 onMounted(async () => {
   const cookie = useCookie('cookie-consent')
@@ -122,14 +81,16 @@ onMounted(async () => {
       collapsible
       resizable
       class="bg-elevated/25"
-      :ui="{ footer: 'lg:border-t lg:border-default' }"
+      :ui="{
+        body: 'pt-4',
+        footer: 'lg:border-t lg:border-default bg-base dark:bg-dark-base'
+      }"
     >
-      <template #header="{ collapsed }">
-        <TeamsMenu :collapsed="collapsed" />
-      </template>
-
       <template #default="{ collapsed }">
-        <UDashboardSearchButton :collapsed="collapsed" class="bg-transparent ring-default" />
+        <NuxtImg
+          class="h-10 self-center w-full"
+          src="/img/dataplay-logo.svg"
+        />
 
         <UNavigationMenu
           :collapsed="collapsed"
@@ -152,8 +113,6 @@ onMounted(async () => {
         <UserMenu :collapsed="collapsed" />
       </template>
     </UDashboardSidebar>
-
-    <UDashboardSearch :groups="groups" />
 
     <slot />
 
