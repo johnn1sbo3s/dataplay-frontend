6ef3dc3291a73f10a068351971a6472f3670e594
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-12-13 16:36:21 -0300

    Implementa segmentedControl

--- app/components/NavigationBar.vue
+++ app/components/NavigationBar.vue
@@ -6,13 +6,15 @@
   >
     <div
       class="
-        glass-panel border-b-0 border-b-white/5 px-6 py-4 my-4 rounded-2xl
-        max-w-[1880px] mx-auto
+        glass-panel border-b-0 border-t-0 border-b-white/5 px-6 py-4 my-4 rounded-2xl
+        max-w-[1880px] mx-auto shadow-l
       "
     >
       <div class="flex items-center justify-between">
-        <div class="flex items-center gap-5">
+        <div class="flex items-center gap-8">
           <BrandLogo />
+
+          <SegmentedControl />
         </div>
       </div>
     </div>
---
+++ app/components/SegmentedControl.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+type NavTab = {
+  value: 'dashboard' | 'fixtures'
+  label: string
+  icon: string
+  to: string
+}
+
+const route = useRoute()
+
+const items = ref<NavTab[]>([
+  {
+    value: 'dashboard',
+    label: 'Dashboard',
+    icon: 'i-lucide-home',
+    to: '/'
+  },
+  {
+    value: 'fixtures',
+    label: 'Jogos',
+    icon: 'i-lucide-calendar-range',
+    to: '/fixtures'
+  }
+])
+
+const activeItem = ref(resolveInitialItem())
+
+watch(() => activeItem.value, () => {
+  const item = items.value?.find((item: NavTab) => item.value === activeItem.value)
+
+  if (item) {
+    navigateTo(item.to)
+  }
+})
+
+function resolveInitialItem() {
+  return items.value?.find((item: NavTab) => item.to === route.fullPath)?.value || 'dashboard'
+}
+</script>
+
+<template>
+  <UTabs
+    v-model="activeItem"
+    :content="false"
+    :items="items"
+    :ui="{
+      list: 'px-5 py-2 bg-white/5 p-1 rounded-full border border-white/5 shadow-s',
+      indicator: 'rounded-full bg-primary text-background-dark shadow-lg shadow-primary/20'
+    }"
+    class="w-full"
+  />
+</template>
