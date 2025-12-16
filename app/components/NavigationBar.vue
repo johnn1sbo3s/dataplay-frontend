5833f32ebdc6730b58bacb23b4cbda61850e58eb
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-12-15 21:22:55 -0300

    Ajusta padding lateral do conteúdo

--- app/components/NavigationBar.vue
+++ app/components/NavigationBar.vue
@@ -7,7 +7,7 @@ function openPortfolio() {
 <template>
   <header
     id="navigation-bar"
-    class="fixed top-0 z-50 left-0 right-0 mx-5"
+    class="fixed top-0 z-50 left-0 right-0 mx-10"
   >
     <div
       class="glass-panel border-b-0 border-t-0 border-b-white/5 px-6 py-4 my-4 rounded-3xl
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="template-default"
-    class="relative flex flex-col min-h-screen px-5 items-center"
+    class="relative flex flex-col min-h-screen px-10 items-center"
   >
     <div class="flex flex-col w-full max-w-[1880px] flex-1">
       <NavigationBar />
