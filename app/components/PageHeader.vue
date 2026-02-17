94c6b906e9ff5d8ab1fa49fedea17f062084f4b7
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-17 11:31:43 -0300

    Ajusta alinhamento do date picker

--- app/components/PageHeader.vue
+++ app/components/PageHeader.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col sm:flex-row sm:items-end justify-between gap-5 sm:gap-4">
+  <div class="flex flex-col sm:flex-row sm:items-end justify-between gap-5 sm:gap-4 lg:w-1/2 lg:gap-8">
     <div>
       <h1 class="text-3xl font-bold text-neutral-200 tracking-tight mb-1">
         {{ title }}
