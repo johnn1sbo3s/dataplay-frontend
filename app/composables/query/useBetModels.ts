b6f0ad484e9d5546acf9dfbfe4d2a294699051f7
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-18 22:54:36 -0300

    Implementa useQuery para as métricas dos modelos

--- app/composables/query/useBetModels.ts
+++ app/composables/query/useBetModels.ts
@@ -1,6 +1,11 @@
 import { useQuery } from '@tanstack/vue-query'
 import type { BetModel, Bet } from '~/types'
 
+interface ResultsByModelParams {
+  initialDate?: string
+  finalDate?: string
+}
+
 export const useBetModels = () => {
   const config = useRuntimeConfig()
   const baseURL = config.public.apiBaseUrl
@@ -11,13 +16,27 @@ export const useBetModels = () => {
   })
 }
 
-export const useBetsByModel = (name: Ref<string>, date: Ref<string>) => {
+export const useBetsByModel = (name: Ref<string | undefined>, date: Ref<string>) => {
   const config = useRuntimeConfig()
   const baseURL = config.public.apiBaseUrl
 
-  console.log('chegou: ', name, date)
   return useQuery({
     queryKey: ['bets-by-model', name, date],
-    queryFn: () => $fetch<{ bets: Bet[] }>(baseURL + `/bet-models/${name.value}/bets?date=${date.value}`)
+    queryFn: () => $fetch<{ bets: Bet[] }>(baseURL + `/bet-models/${name.value}/bets?date=${date.value}`),
+    enabled: computed(() => !!name.value)
+  })
+}
+
+export const useResultsByModel = (name: Ref<string | undefined>, params?: Ref<ResultsByModelParams>) => {
+  const config = useRuntimeConfig()
+  const baseURL = config.public.apiBaseUrl
+
+  return useQuery({
+    queryKey: ['results-by-model', name, params],
+    queryFn: () => $fetch<{ results: BetModel[] }>(baseURL + `/bet-models/${name.value}/historical-metrics`, {
+      method: 'GET',
+      params: params?.value
+    }),
+    enabled: computed(() => !!name.value)
   })
 }
